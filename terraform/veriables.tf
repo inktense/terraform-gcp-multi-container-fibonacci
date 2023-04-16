6fc83a7afe7517d7799c543460707e55971044fa
Bianca Popescu <inktense12@gmail.com>
2023-04-16 17:59:39 +0100

    Added gcp provider

---
+++ terraform/veriables.tf
@@ -0,0 +1,11 @@
+variable "gcp_region" {
+  description = "Region where main resources should be created."
+  type        = string
+  default     = "europe-west2"
+}
+
+variable "project_name" {
+  description = "Name of the project created."
+  type        = string
+  default     = "k8s-multi-container-fibonacci"
+}
---
+++ terraform/version.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_version = "= 1.4.2"
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.61.0"
+    }
+  }
+}
